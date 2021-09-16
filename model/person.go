d5b84a10f98ff30998484181ec7720b9632c148f
kzuniga <kzuniga@falabella.cl>
2021-09-16 17:49:24 -0300

    [KZ] Commit initial

---
+++ model/person.go
@@ -0,0 +1,24 @@
+package model
+
+// Estructura de una comunidad
+type Community struct {
+	// Name nombre de una comunidad. Ej Edteam
+	Name string
+}
+
+// Communities slice de comunidades
+type Comunities []Community
+
+// Person Estructura de una persona
+type Person struct {
+	// Name : nombre de una persona. Ej Karen
+	Name string
+
+	//Age edad de la persona
+	Age uint8
+
+	//Communities comunidades a las que pertenece una persona.
+	Communities Communities
+}
+
+type Persons []Person
---
+++ storage/memory.go
@@ -0,0 +1,10 @@
+package storage;
+
+
+// Esta estructura me va a permitir almacenar en memoria un mapa de enteros de personas, para identificar a una persona por id.
+type Memory structure {
+	currentID int
+
+	Person map[int]model.Person	
+
+}
